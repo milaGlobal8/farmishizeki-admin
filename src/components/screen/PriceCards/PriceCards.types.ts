89a27aed06c9d9d3aec1d1e4da76136e879440c6
ShunIshizeki <shun2000c9@outlook.jp>
2024-01-25 11:17:12 +0900

    PriceCardsの型定義を作成

---
+++ src/components/screen/PriceCards/PriceCards.types.ts
@@ -0,0 +1,10 @@
+import { Vegs } from "../../../types/Vegs.types";
+
+export type PriceCardsProps = {
+  vegs: Vegs[];
+};
+
+export type PriceCardsInputProps = {
+  aGramStock?: number;
+  bGramStock?: number;
+};
